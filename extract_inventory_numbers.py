ae94a97160777c06819d0a5018558f981cc47c8d
Henry R <90934334+henry-r18@users.noreply.github.com>
2025-04-10 18:17:25 -0400

    SYS-1783: Add script to extract inventory numbers from FTVA spreadsheet (#13)
    * Update script to account for adjusted FTVA specs and add test cases
    * Update regex, extract function, and test
    * Fix issue where matches returned out of order
    * Fix issue where None returned instead of ''
    * Update regex to account for new edge cases
    * Add tests for new edge cases
    * Add logic to remove known false positives
    * Update relevant test case
    * Update README

---
+++ extract_inventory_numbers.py
@@ -0,0 +1,89 @@
+import argparse
+import re
+import pandas as pd
+from pathlib import Path
+
+
+def _get_args() -> argparse.Namespace:
+    """Returns the command-line arguments for this program."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_file",
+        help="Path to XLSX spreadsheet provided by FTVA",
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
+
+
+def _compile_regex() -> re.Pattern:
+    # Return a compiled RegEx pattern for matching inventory numbers
+    # the RegEx is comprised of three main parts, which are explained inline below
+    #
+    # NOTE: the pattern works in most cases, but will return false positives
+    # where the input contains a substring that syntactically matches pattern
+    # but is not actually a valid inventory number, according to FTVA
+    # e.g. AAC442\Title_T01ASYNC_Surround matches T01, which is a valid pattern,
+    # but not an actual inventory number
+    regex_components = [
+        r'(?<![A-Z])',  # pattern not preceded by capital letter, to mitigate false positives
+        r'(?:M|T|DVD|FE|HFA|VA|XFE|XFF|XVE)',  # non-capturing group of 8 prefixes defined by FTVA
+        r'\d{2,}',  # 2 or more digits, as many as possible
+        r'(?:[A-Z](?![A-Za-z]))?'  # optional suffix 1 capital letter not followed by another letter
+    ]
+
+    return re.compile(''.join(regex_components))
+
+
+def _extract_inventory_numbers(
+        value: str,
+        inventory_number_pattern: re.Pattern = _compile_regex()
+) -> str:
+    """Returns a pipe-delimited list of matches against provided pattern."""
+    if not isinstance(value, str):
+        return
+
+    matches = re.findall(inventory_number_pattern, value)
+    if matches:
+        # uses dict.fromkeys() to get unique values
+        # while maintaining list order
+        unique_inventory_numbers = list(dict.fromkeys(matches))
+
+        # NOTE: hard-coding a list of known false positives here
+        # i.e. strings that match pattern but are known to not be actual inv #s
+        # this could be made into a script argument later
+        #
+        # if such values exist in list of unique inv #s, remove them
+        known_false_positives = ["T01"]
+        for false_positive in known_false_positives:
+            if false_positive in unique_inventory_numbers:
+                unique_inventory_numbers.remove(false_positive)
+
+        # per FTVA spec, provide pipe-delimited string
+        # if multiple matches in a single input value
+        return '|'.join(unique_inventory_numbers)
+    return ''
+
+
+def main() -> None:
+    """Extracts inventory numbers from values in a spreadsheet provided by FTVA digital lab."""
+    args = _get_args()
+
+    input_file_path = Path(args.data_file)
+    if not input_file_path.exists():
+        raise FileExistsError("Data file does not exist")
+
+    # The target sheet and column are hard-coded here
+    # might want to move to set them up as arguments later
+    df = pd.read_excel(args.data_file, sheet_name='Tapes(row 4560-24712)')
+    # Using Pandas Series.apply() method here to apply function to target column
+    # and store results in a new column
+    df["Inventory Number [EXTRACTED]"] = df["Legacy Path"].apply(_extract_inventory_numbers)
+
+    # Save new XLSX file with extracted inventory numbers
+    output_path = Path(args.data_file).with_stem(input_file_path.stem + "_with_inventory_numbers")
+    df.to_excel(output_path, index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_extract_inventory_numbers.py
@@ -0,0 +1,47 @@
+import unittest
+from extract_inventory_numbers import _extract_inventory_numbers
+
+
+class TestRegEx(unittest.TestCase):
+
+    def test_regex(self):
+        # test cases comprised of tuples
+        # where the first item is the input string
+        # and the second item is the expected output derived from FTVA specs
+        test_cases = (
+            ("HFA27M_Reel", "HFA27M"),
+            ("VA13161T_KTLA", "VA13161T"),
+            ("M190816Medea2", "M190816"),
+            ("DVD13360_HouseOfCats_FromDVD_SD_2997FPS_VOB", "DVD13360"),
+            ("GeraldMcBoingBoingShow_T119482_DerTeamfromZwisendorpff", "T119482"),
+            ("XFE1915MX", "XFE1915"),  # invalid suffix case
+            ("Randy_Requiem1", ""),  # no matching inv # in this case, so should be empty string
+            (
+                "XFE4098M_XFF104M_DamagedLives_Finals",
+                "XFE4098M|XFF104M"
+            ),  # multi-match to pipe-delimited string case
+            (
+                "XVE779T_ZVE780T_OneNightStand_WorldOfLennyBruce_CaptureFiles_SD_2997FPS_YUV",
+                "XVE779T"
+            ),  # XVE is a valid prefix, but ZVE isn't, so only 1 valid inv # in input
+            ("Max_From_DVD_H264", ""),  # input has pattern look-alike (H264), but no valid inv #
+            ("HARVEST3000AUDIOMAG", ""),  # like last case, look-alike in middle, but no valid inv #
+            (
+                "M46293SanFernandoCLEANEXPORT3",
+                "M46293"
+            ),  # contains valid inv # (M46293), but also look-alike (T3)
+            # flake8 flagged escape seq (\T), hence raw strings in following cases
+            (
+                r"AAC424\T70123_50Years_Kids_Programming\T70123_50Years_T1",
+                "T70123"
+            ),  # per FTVA, inv #s have 2 or more digits, so T70123 is good, but T1 is invalid
+            (
+                r"AAC442\Title_T01ASYNC_Surround",
+                ""
+            )  # known false positive--T01 is syntactically valid, but not actual inv #, per FTVA
+               # script should remove these
+        )
+
+        for input, output in test_cases:
+            inventory_numbers = _extract_inventory_numbers(input)
+            self.assertEqual(inventory_numbers, output)
