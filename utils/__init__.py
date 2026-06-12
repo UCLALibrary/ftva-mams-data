4b82c51e588cf948be1316654f1daa944aeb6edb
Zoe Tucker <zoetucker@library.ucla.edu>
2026-06-12 09:26:15 -0700

    update __init__.py docstring w/implementation reasoning

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,16 @@
 """
 Shared utility modules for ftva-mams-data scripts.
+This package does not re-export functions from submodules. Callers must write imports like:
+
+from utils.filemaker_utils import configure_logging
+
+instead of
+
+from utils import configure_logging
+
+This is done to avoid loading spaCy in contexts where we don't need it.
+Since __init__.py is executed on import, if we re-exported the spacy_utils functions here,
+spaCy would be loaded whenever any utils function is imported, including those in other modules.
 
 Modules
 -------
