8a43550a1944aaa7e596b64b0fff1857d3f6277d
Henry Rosen <herosen@library.ucla.edu>
2026-06-18 17:10:02 -0700

    Update tests to cover refactor of `generate_metadata.py`
    - Add unit test for new `_set_record_type_and_match_asset` function
    - Update imports and existing tests to use new function names

--- tests/test_generate_metadata.py
+++ tests/test_generate_metadata.py
@@ -3,8 +3,9 @@ import io
 import logging
 
 from generate_metadata import (
-    _set_record_type,
-    _update_match_record_type,
+    _set_record_type_and_match_asset,
+    _validate_match_asset_relationships,
+    _compare_inventory_numbers,
     logger,
 )
 
@@ -36,27 +37,55 @@ class TestGenerateMetadata(unittest.TestCase):
             handler.close()
             self.logger.removeHandler(handler)
 
-    def test_set_record_type_with_match_asset(self):
-        """Test that `_set_record_type` sets the `record_type` correctly
-        when there is a `match_asset` relationship.
-        """
-        # Tuple of test records and expected results
-        test_records = [
+    def test_set_record_type_and_match_asset(self):
+        """Test that `_set_record_type_and_match_asset`
+        sets `record_type` and `match_asset` correctly."""
+        test_digital_data_records = [
             {
                 "uuid": "12345",
                 "inventory_numbers": ["INV001"],
-            },  # should be asset
+            },
             {
                 "uuid": "67890",
                 "inventory_numbers": ["INV002"],
-            },  # should be asset
-            {
-                "uuid": "09876",
-                "inventory_numbers": ["INV001"],
-                "match_asset": "12345",
-            },  # since inventory numbers match the target match_asset, should be track
+                "incoming_relationships": [
+                    {
+                        "relationship_type": "isTrackOf",
+                        "source_uuid": "12345",
+                    }
+                ],
+            },  # track relationship indicated in DD record targeting asset 12345
         ]
         expected_results = [
+            {
+                "uuid": "12345",
+                "inventory_numbers": ["INV001"],
+                "record_type": "asset",
+            },  # this output should be an asset
+            {
+                "uuid": "67890",
+                "inventory_numbers": ["INV002"],
+                "record_type": "track",
+                "match_asset": "12345",
+            },  # this output should be a track, with match_asset set to UUID of the asset
+        ]
+
+        # Zip together the two lists to get tuples of (dd_record, expected_result)
+        for dd_record, expected_result in zip(
+            test_digital_data_records, expected_results
+        ):
+            with self.subTest(dd_record=dd_record, expected_result=expected_result):
+                metadata_record = _set_record_type_and_match_asset(
+                    dd_record, expected_result
+                )
+                self.assertEqual(metadata_record, expected_result)
+
+    def test_validate_match_asset_relationships_with_valid_match_asset(self):
+        """Test that `_validate_match_asset_relationships`
+        leaves the `record_type` and `match_asset` values unchanged
+        when the `match_asset` relationship is valid.
+        """
+        test_records = [
             {
                 "uuid": "12345",
                 "inventory_numbers": ["INV001"],
@@ -72,31 +101,17 @@ class TestGenerateMetadata(unittest.TestCase):
                 "inventory_numbers": ["INV001"],
                 "match_asset": "12345",
                 "record_type": "track",
-            },
+            },  # valid match_asset relationship, so should be unchanged
         ]
 
-        result_records = _set_record_type(test_records)
-        self.assertEqual(result_records, expected_results)
+        result_records = _validate_match_asset_relationships(test_records)
+        self.assertEqual(result_records, test_records)
 
-    def test_set_record_type_without_match_asset(self):
-        """Test that `_set_record_type` sets the `record_type` correctly
-        when there is no `match_asset` relationship.
+    def test_validate_match_asset_relationships_without_match_asset(self):
+        """Test that `_validate_match_asset_relationships`
+        sets the `record_type` correctly when there is no `match_asset` relationship.
         """
         test_records = [
-            {
-                "uuid": "12345",
-                "inventory_numbers": ["INV001"],
-            },  # should be asset
-            {
-                "uuid": "67890",
-                "inventory_numbers": ["INV002"],
-            },  # should be asset
-            {
-                "uuid": "09876",
-                "inventory_numbers": ["INV001"],
-            },  # since there is no match_asset relationship, should be asset
-        ]
-        expected_results = [
             {
                 "uuid": "12345",
                 "inventory_numbers": ["INV001"],
@@ -114,43 +129,37 @@ class TestGenerateMetadata(unittest.TestCase):
             },
         ]
 
-        result_records = _set_record_type(test_records)
-        self.assertEqual(result_records, expected_results)
+        result_records = _validate_match_asset_relationships(test_records)
+        # All test records should remain `assets`, since there is no match_asset relationship.
+        self.assertEqual(result_records, test_records)
 
-    def test_set_record_type_when_match_asset_is_missing(self):
-        """Test that `_set_record_type` skips records
-        when the `match_asset` cannot be found and logs an error.
+    def test_validate_match_asset_relationships_when_match_asset_is_missing(self):
+        """Test that `_validate_match_asset_relationships` logs an error
+        when the targeted `match_asset` record is not in the batch.
         """
         test_records = [
-            {"uuid": "12345", "inventory_numbers": ["INV001"]},
-            {"uuid": "67890", "inventory_numbers": ["INV002"]},
-            {
-                "uuid": "09876",
-                "inventory_numbers": ["INV001"],
-                "match_asset": "34567",
-            },  # should be skipped, because target match_asset does not exist
-        ]
-        expected_results = [
             {"uuid": "12345", "inventory_numbers": ["INV001"], "record_type": "asset"},
             {"uuid": "67890", "inventory_numbers": ["INV002"], "record_type": "asset"},
             {
                 "uuid": "09876",
                 "inventory_numbers": ["INV001"],
-                "match_asset": "34567",
+                "match_asset": "34567",  # missing from "batch", i.e. `test_records`
+                "record_type": "track",
             },  # there should be an error in logs about the missing match_asset
         ]
 
-        result_records = _set_record_type(test_records)
-        self.assertEqual(result_records, expected_results)
-        # `_set_record_type` should log an error about the missing match_asset.
+        result_records = _validate_match_asset_relationships(test_records)
+        # Test records should remain unchanged
+        self.assertEqual(result_records, test_records)
+        # `_validate_match_asset_relationships` should log an error about the missing match_asset.
         # Check for it here in the test IO stream being used as the test log handler.
         self.assertIn(
-            "ERROR: Match asset 34567 for record 09876 not found in metadata records.",
+            "ERROR: Match asset 34567 for record 09876 not found in batch.",
             self.stream.getvalue(),
         )
 
-    def test_update_match_record_type_with_valid_match_asset(self):
-        """Test that `_update_match_record_type` updates the `record_type` correctly
+    def test_compare_inventory_numbers_with_valid_match_asset(self):
+        """Test that `_compare_inventory_numbers` updates the `record_type` correctly
         when there is a valid `match_asset` relationship.
         """
         # Test cases comprised of (record with match, matched record, expected_result)
@@ -211,7 +220,7 @@ class TestGenerateMetadata(unittest.TestCase):
         for test_case in test_cases:
             with self.subTest(test_case=test_case):
                 test_record, test_matched_record, expected_result = test_case
-                result_record = _update_match_record_type(
+                result_record = _compare_inventory_numbers(
                     test_record, test_matched_record
                 )
                 self.assertEqual(result_record, expected_result)
