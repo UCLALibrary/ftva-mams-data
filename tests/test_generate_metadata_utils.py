3af553f46cc74807465456f94dc66116f2be26e7
Henry Rosen <herosen@library.ucla.edu>
2026-07-08 13:26:16 -0700

    Rename `test_generate_metadata.py` to `test_generate_metadata_utils.py`
    - Refactor tests that cover `validate_match_asset_relationships` to improve handling of logging assertions

--- tests/test_generate_metadata_utils.py
+++ tests/test_generate_metadata_utils.py
@@ -1,42 +1,13 @@
 import unittest
-import io
-import logging
 
-from generate_metadata import (
-    _validate_match_asset_relationships,
-    logger,
-)
+from utils.generate_metadata_utils import validate_match_asset_relationships
 
 
 class TestGenerateMetadata(unittest.TestCase):
-    """Test the `generate_metadata` module."""
-
-    def setUp(self):
-        # The logger used in these tests is imported from `generate_metadata.py`,
-        # so this setUp method clears existing file and console handlers,
-        # then sets up a new test handler that captures the messages emitted in `generate_metadata`
-        # to an IO stream, which can then be used in tests, without log files or console output.
-        for handler in logger.handlers:
-            handler.close()
-            logger.removeHandler(handler)
-
-        stream = io.StringIO()
-        test_handler = logging.StreamHandler(stream)
-        formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-        test_handler.setFormatter(formatter)
-        logger.addHandler(test_handler)
-
-        self.logger = logger
-        self.stream = stream
-
-    def tearDown(self):
-        # Reset the test handler after each test, to avoid multiple streams being created.
-        for handler in self.logger.handlers:
-            handler.close()
-            self.logger.removeHandler(handler)
+    """Tests for `utils.generate_metadata_utils`."""
 
     def test_validate_match_asset_relationships_with_valid_match_asset(self):
-        """Test that `_validate_match_asset_relationships`
+        """Test that `validate_match_asset_relationships`
         returns True when all match_asset relationships are valid.
         """
         test_records = [
@@ -57,11 +28,11 @@ class TestGenerateMetadata(unittest.TestCase):
                 "record_type": "track",
             },  # valid match_asset relationship: both match_asset and inv no match target
         ]
-        result = _validate_match_asset_relationships(test_records)
+        result = validate_match_asset_relationships(test_records)
         self.assertTrue(result)
 
     def test_validate_match_asset_relationships_without_match_asset(self):
-        """Test that `_validate_match_asset_relationships`
+        """Test that `validate_match_asset_relationships`
         returns True when there are no `match_asset` relationships.
         """
         test_records = [
@@ -82,11 +53,11 @@ class TestGenerateMetadata(unittest.TestCase):
             },
         ]
 
-        result = _validate_match_asset_relationships(test_records)
+        result = validate_match_asset_relationships(test_records)
         self.assertTrue(result)
 
     def test_validate_match_asset_relationships_when_match_asset_is_missing(self):
-        """Test that `_validate_match_asset_relationships`
+        """Test that `validate_match_asset_relationships`
         returns False and logs expected error
         when the targeted `match_asset` record is missing.
         """
@@ -101,16 +72,16 @@ class TestGenerateMetadata(unittest.TestCase):
             },
         ]
 
-        result = _validate_match_asset_relationships(test_records)
-        self.assertFalse(result)
-        # There should be an error in logs about the missing target
-        self.assertIn(
-            "ERROR: Match asset 34567 for record 09876 not found in batch.",
-            self.stream.getvalue(),
-        )
+        with self.assertLogs(level="ERROR") as context_manager:
+            result = validate_match_asset_relationships(test_records)
+            self.assertFalse(result)
+            self.assertIn(
+                "Match asset 34567 for record 09876 not found in batch.",
+                context_manager.output[0],
+            )
 
     def test_validate_match_asset_relationships_when_inv_nos_do_not_match(self):
-        """Test that `_validate_match_asset_relationships`
+        """Test that `validate_match_asset_relationships`
         returns False and logs expected error
         when the inventory numbers of the match_asset and target record do not match.
         """
@@ -125,12 +96,11 @@ class TestGenerateMetadata(unittest.TestCase):
             },
         ]
 
-        result = _validate_match_asset_relationships(test_records)
-        self.assertFalse(result)
-        self.assertIn(
-            (
-                "ERROR: Inventory numbers do not match for match_asset relationship "
-                "track 09876: 'INV003', asset 12345: 'INV001'"
-            ),
-            self.stream.getvalue(),
-        )
+        with self.assertLogs(level="ERROR") as context_manager:
+            result = validate_match_asset_relationships(test_records)
+            self.assertFalse(result)
+            self.assertIn(
+                "Inventory numbers do not match for match_asset relationship "
+                "track 09876: 'INV003', asset 12345: 'INV001'",
+                context_manager.output[0],
+            )
